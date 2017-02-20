ac64eb3d69f23d5a9cab1ea8da94ba94a8ffbdf4
filipe paiva <filipe.p@soltroiavillas.com>
2017-02-20 16:37:00 +0000

    just model no migrations

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,6 +1,7 @@
 class Transaction < ApplicationRecord
-  belongs_to :user #as buyer_id
-  belongs_to :product_offers #will get a user Id through
+  belongs_to :user, foreign_key: "buyer_id"
+  belongs_to :product_offers
+
   validates :delivery_address, presence: true
   validates :return_address, true
 end
