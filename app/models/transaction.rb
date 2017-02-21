a5780eace9fb0efa061f1b90b588abfad41eb7b9
Diogo Farias Rodrigues <diogofgr@gmail.com>
2017-02-21 13:37:29 +0000

    Seed database with 3 of each

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,6 +1,6 @@
 class Transaction < ApplicationRecord
   belongs_to :user, foreign_key: "buyer_id"
-  belongs_to :product_offers
+  belongs_to :product_offer
 
   validates :delivery_address, presence: true
   validates :return_address, presence: true
