690a586d2b8f6c21b5acedcda3c8fd2e9248c7c9
Diogo Farias Rodrigues <diogofgr@gmail.com>
2017-02-22 15:41:22 +0000

    Add product search in home page

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,13 @@
 class Product < ApplicationRecord
   has_many :product_offers
+
+  def self.search(search)
+    where("name ILIKE ?", "%#{search}%")
+    # where("name ILIKE ?", "%bat%")
+
+  end
+
+# Use this ine to search in other fields, not only name;
+# where("name LIKE ? OR ingredients LIKE ? OR cooking_instructions LIKE ?", "%#{search}%", "%#{search}%", "%#{search}%", "%#{search}%")
+
 end
